13976f4378e2d3865ac6ac1cd8396ddb567aa2bc
sagarmakwana <makwana_sagar@outlook.com>
2016-03-05 16:37:53 -0800

    -Complete Revamp -KB generated -Standardization done

--- hw2cs561s16.py
+++ hw2cs561s16.py
@@ -1,187 +1,72 @@
-#@author Sagar Makwana
+class KNOWLEDGE_BASE:
+    ruleCount = 0
 
-#---------------------------------------Function Definitions------------------------------------------------
+    def __init__(self,listofRules):
+        KNOWLEDGE_BASE.ruleCount = len(listofRules)
+        self.rules = listofRules[:]
 
-#Converts the given rule from the input KNOWLEDGE BASE to standard dictionary format.
-def convertRuleToDict(rule):
-    #The dictionary that is to be returned after converting the rule into dictionary.
-    ruleDict = ''
+    def standardize_knowledge_base(self):
 
-    implicationSplit = rule.split(' => ')
+        openIndex = -1
+        closeIndex = -1
 
-    #If it is a complex sentence
-    if len(implicationSplit) == 2:
-        #Premise Section
-        conjunctionSplit = implicationSplit[0].split(" && ")
-        ruleDict = {'premiseCount':len(conjunctionSplit)}
+        ruleCount = 1
+        for rule in self.rules:
+            remaining = rule[:]
+            newRule = ''
+            openIndex = remaining.find('(')
+            closeIndex = remaining.find(')')
 
-        for i in range(0,len(conjunctionSplit)):
-            premise = conjunctionSplit[i].strip()
+            while (openIndex != -1 and closeIndex != -1):
+                variables = remaining[openIndex+1:closeIndex]
+                newRule = newRule + remaining[0:openIndex]
+                remaining = remaining[closeIndex+1:]
 
-            if premise[0] == '~':
-                ruleDict['premise'+str(i+1)] = {'not': True}
-                premise = premise[1:]
+                variables = variables.split(',')
+
+                subsVar = ''
+                for l in range (0,len(variables)):
+                    variables[l] = variables[l].strip()
+                    if ord(str(variables[l][0])) >= 97:
+                        subsVar = subsVar + variables[l]+''+str(ruleCount)+', '
+                    else:
+                        subsVar = subsVar + variables[l] + ', '
+
+
+                subsVar = '(' + subsVar[0:len(subsVar)-2] + ')'
+                newRule = newRule + subsVar
+
+                openIndex = remaining.find('(')
+                closeIndex = remaining.find(')')
+
+            self.rules[ruleCount-1] = newRule
+            ruleCount = ruleCount + 1
+
+    def fetch_rules_for_goal(self,goal):
+        goalName = goal[0:goal.index('(')]
+
+        #List of rules that match with goal
+        fetched_rules = []
+
+        for rule in self.rules:
+            implicationSplit = rule.split(' => ')
+
+            name = ''
+            if len(implicationSplit) == 2:
+                consequent = implicationSplit[1].strip()
+                name = consequent[0:consequent.index('(')]
             else:
-                ruleDict['premise'+str(i+1)] = {'not': False}
+                name = rule[0:rule.index('(')]
 
-            name = premise[0:premise.index('(')]
-            variables = premise[premise.index('(')+1:premise.index(')')]
-            variables = variables.split(',')
-            for l in range (0,len(variables)):
-                variables[l] = variables[l].strip()
-            varCount = len(variables)
+            if name == goalName:
+                fetched_rules.append(rule)
 
-            ruleDict['premise'+str(i+1)]['varCount'] = varCount
-            ruleDict['premise'+str(i+1)]['variables'] = variables
-            ruleDict['premise'+str(i+1)]['variableSubs'] = variables[:]
-            ruleDict['premise'+str(i+1)]['name'] = name
+        return fetched_rules
 
-        #Conclusion Section
-        conclusion = implicationSplit[1].strip()
+    def fetch_all_rules(self):
+        return self.rules
 
-        if conclusion[0] == '~':
-            ruleDict['conclusion'] = {'not': True}
-            conclusion = conclusion[1:]
-        else:
-            ruleDict['conclusion'] = {'not': False}
-
-        name = conclusion[0:conclusion.index('(')]
-        variables = conclusion[conclusion.index('(')+1:conclusion.index(')')]
-        variables = variables.split(',')
-        for l in range (0,len(variables)):
-            variables[l] = variables[l].strip()
-        varCount = len(variables)
-
-        ruleDict['conclusion']['varCount'] = varCount
-        ruleDict['conclusion']['variables'] = variables
-        ruleDict['conclusion']['variableSubs'] = variables[:]
-        ruleDict['conclusion']['name'] = name
-
-
-    #If it is an atomic sentence
-    elif len(implicationSplit) == 1:
-
-        ruleDict = {'premiseCount': 0}
-
-        conclusion = implicationSplit[0].strip()
-
-        if conclusion[0] == '~':
-            ruleDict['conclusion'] = {'not': True}
-            conclusion = conclusion[1:]
-        else:
-            ruleDict['conclusion'] = {'not': False}
-
-        name = conclusion[0:conclusion.index('(')]
-        variables = conclusion[conclusion.index('(')+1:conclusion.index(')')]
-        variables = variables.split(',')
-        for l in range (0,len(variables)):
-            variables[l] = variables[l].strip()
-        varCount = len(variables)
-
-        ruleDict['conclusion']['varCount'] = varCount
-        ruleDict['conclusion']['variables'] = variables
-        ruleDict['conclusion']['variableSubs'] = variables[:]
-        ruleDict['conclusion']['name'] = name
-
-    return ruleDict
-
-def convertGoalInferenceToDict(goalInference):
-    conjunctionSplit = goalInference.split(" && ")
-
-    ruleDict = {'premiseCount':len(conjunctionSplit)}
-
-    for i in range(0,len(conjunctionSplit)):
-        premise = conjunctionSplit[i].strip()
-
-        if premise[0] == '~':
-            ruleDict['premise'+str(i+1)] = {'not': True}
-            premise = premise[1:]
-        else:
-            ruleDict['premise'+str(i+1)] = {'not': False}
-
-        name = premise[0:premise.index('(')]
-        variables = premise[premise.index('(')+1:premise.index(')')]
-        variables = variables.split(',')
-        varCount = len(variables)
-
-        ruleDict['premise'+str(i+1)]['varCount'] = varCount
-        ruleDict['premise'+str(i+1)]['variables'] = variables
-        ruleDict['premise'+str(i+1)]['variableSubs'] = variables[:]
-        ruleDict['premise'+str(i+1)]['name'] = name
-
-    return ruleDict
-
-
-#TODO: Think on return values
-def BackChainingOR(KNOWLEDGE_BASE,goalConclusion):
-
-    unifiedVariables = ''
-    isUnifiable = False
-    #To check if any of the rules is unifiable
-    ruleUnifiable = False
-
-    for rule in KNOWLEDGE_BASE:
-        if rule['conclusion']['name'] == goalConclusion['name']:
-            unifiedVariables,isUnifiable = unify(rule['conclusion'],goalConclusion)
-            if isUnifiable:
-                rule['conclusion']['variableSubs']=unifiedVariables
-                for i in range (0,rule['conclusion']['varCount']):
-                    if ord(str(rule['conclusion']['variableSubs'][i][0])) < 97:
-                        for j in range (0,rule['premiseCount']):
-                            for k in range(0,rule['premise'+str(j+1)]['varCount']):
-                                if rule['premise'+str(j+1)]['variables'][k] == rule['conclusion']['variables'][i]:
-                                    rule['premise'+str(j+1)]['variableSubs'][k] = rule['conclusion']['variableSubs'][i]
-                #print 'OR:',rule
-                ruleUnifiable = BackChainingAnd(KNOWLEDGE_BASE,rule)
-
-
-    return ruleUnifiable
-
-def BackChainingAnd(KNOWLEDGE_BASE,rule):
-    premiseCount = rule['premiseCount']
-    allPremiseTrue = True
-    if premiseCount == 0:
-        return True
-    else:
-        for i in range(0,premiseCount):
-            allPremiseTrue = allPremiseTrue and BackChainingOR(KNOWLEDGE_BASE,rule['premise'+str(i+1)])
-
-        return allPremiseTrue
-
-
-
-
-#This function unifies the given ruleConclusion and the goalConclusion and returns the
-# unified substitution of variables and also returns whether they can be unifies or not.
-# @return unifiedVariables : New unified substitution
-# @return isUnifiable : Boolean stating whether unification is possible or not
-def unify(ruleConclusion,goalConclusion):
-    ruleConclusionVariables = ruleConclusion['variableSubs']
-    goalConclusionVariables = goalConclusion['variableSubs']
-    unifiedVariables = []
-
-
-    for i in range (0,ruleConclusion['varCount']):
-        if ord(str(goalConclusionVariables[i])[0]) >= 97 :
-            unifiedVariables.append(ruleConclusionVariables[i])
-        elif ord(str(ruleConclusionVariables[i])[0]) >= 97 and ord(str(goalConclusionVariables[i])[0]) < 97:
-            unifiedVariables.append(goalConclusionVariables[i])
-        elif str(ruleConclusionVariables[i]) == str(goalConclusionVariables[i]):
-            unifiedVariables.append(ruleConclusionVariables[i])
-        else:
-            return unifiedVariables,False
-
-    return unifiedVariables,True
-
-
-
-
-
-
-
-
-
-#----------------------------------------Input and Control--------------------------------------------------
+#----------------------------------------Input and Control-------------------------------------
 
 #filename = sys.argv[-1]
 filename = 'input.txt'
@@ -190,23 +75,33 @@ filename = 'input.txt'
 inputFile = open(filename)
 
 #Reading the goal Inference
-goalInference = inputFile.readline().strip()
-goalInference = convertGoalInferenceToDict(goalInference)
-print goalInference
+goalInput = inputFile.readline().strip()
 
-#Reading the Knowledge Base
+goals = goalInput.split(" && ")
+
+for i in range(0,len(goals)):
+    goals[i] = goals[i].strip()
+
+
 kbCount = int(inputFile.readline().strip())
-KNOWLEDGE_BASE = []
+kb = []
 
 for i in range(0,kbCount):
     rule = inputFile.readline().strip()
-    KNOWLEDGE_BASE.append(convertRuleToDict(rule))
-    print KNOWLEDGE_BASE[i]
+    kb.append(rule)
 
-print 'Test operations:'
-print BackChainingOR(KNOWLEDGE_BASE,goalInference['premise1'])
-print KNOWLEDGE_BASE
-'''x,y = unify(KNOWLEDGE_BASE[3]['conclusion'],KNOWLEDGE_BASE[0]['premise3'])
-print x
-print y
-'''
+print 'Before'
+KB = KNOWLEDGE_BASE(kb)
+
+print KB.fetch_all_rules()
+
+KB.standardize_knowledge_base()
+
+print 'After'
+
+print  KB.fetch_all_rules()
+
+
+
+
+
